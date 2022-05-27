bac71ee0fa1be54fabf18d8dd10d4ce7b20b8140
vuongle2609 <vuongle2609@gmail.com>
2022-05-28 01:16:38 +0700

    added related manga, fixed pagination, fixed no alttitles

--- components/Manga/MangaArt/MangaArt.tsx
+++ components/Manga/MangaArt/MangaArt.tsx
@@ -33,7 +33,7 @@ const MangaArt: FC<PropsType> = ({ styles, mangaData }) => {
   const dataArr = data?.data?.sort((a: any, b: any) => {
     return Number(a.attributes.volume) - Number(b.attributes.volume);
   });
-
+  
   return (
     <div className={styles["manga-sub-content"]}>
       <Grid.Container
@@ -41,42 +41,46 @@ const MangaArt: FC<PropsType> = ({ styles, mangaData }) => {
           flexDirection: "row",
         }}
       >
-        {data &&
-          dataArr.map((item: any, index: number) => (
-            <Grid
-              xl={2}
-              lg={2}
-              md={2.4}
-              sm={3}
-              xs={6}
-              css={{
-                display: "flex",
-                flexDirection: "column",
-                padding: "$3 $3",
-              }}
-              key={index}
-            >
-              <Cover
-                coverUrl={getSingleCover({
-                  id: mangaData.id,
-                  quality: true,
-                  fileName: item.attributes.fileName,
-                })}
-              />
-              <Text size={16} css={{ marginTop: "$1" }} b>
-                Volume {item.attributes.volume}
-              </Text>
-            </Grid>
-          ))}
-
-        {!data && "loading"}
+        {data
+          ? dataArr.map((item: any, index: number) => (
+              <Grid
+                xl={2}
+                lg={2}
+                md={2.4}
+                sm={3}
+                xs={6}
+                css={{
+                  display: "flex",
+                  flexDirection: "column",
+                  padding: "$3 $3",
+                }}
+                key={index}
+              >
+                <Cover
+                  coverUrl={getSingleCover({
+                    id: mangaData.id,
+                    quality: true,
+                    fileName: item.attributes.fileName,
+                  })}
+                />
+                <Text size={16} css={{ marginTop: "$1" }} b>
+                  Volume {item.attributes.volume}
+                </Text>
+              </Grid>
+            ))
+          : "There's no art"}
         {error && "Couldn't load"}
       </Grid.Container>
       <div className={styles["manga-pagination"]}>
-        <Pagination
-          total={Math.ceil(data?.total / 12)}
-          onChange={(num: number) => setOffset(12 * (num - 1))}
-        ></Pagination>
+        {data ? (
+          <Pagination
+            page={offset / 12 + 1}
+            total={Math.ceil(data?.total / 12)}
+            onChange={(num: number) => setOffset(12 * (num - 1))}
+          ></Pagination>
+        ) : (
+          false
+        )}
       </div>
     </div>
   );
